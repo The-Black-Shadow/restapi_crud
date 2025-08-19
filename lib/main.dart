fc4f09f5c6bb5a1fdad7196c07fa2431332769c2
the-black-shadow <mehedihasan.cse18@gmail.com>
2025-08-19 23:29:00 +0600

    added test commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:restapi_crud/core/di/service_locator.dart';
 import 'package:restapi_crud/my_app.dart';
 
 void main() {
+  //initiate dependency injection
   setupLocator();
   runApp(const MyApp());
 }
