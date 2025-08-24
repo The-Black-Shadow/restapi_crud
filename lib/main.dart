8f858d7bd595cee8a0ee647e09cb0118c821d199
the-black-shadow <mehedihasan.cse18@gmail.com>
2025-08-24 16:02:07 +0600

    added bloc oberver for seeing bloc state change and error

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restapi_crud/bloc_observer.dart';
 import 'package:restapi_crud/core/di/service_locator.dart';
 import 'package:restapi_crud/my_app.dart';
 
 void main() {
+  //bloc observer
+  Bloc.observer = MyBlocObserver();
   //initiate dependency injection
   setupLocator();
   runApp(const MyApp());
